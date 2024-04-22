d9d8ce2219dfb8b316981146a5ec174beb88585f
Marco De Felice <=>
2024-04-22 15:33:16 +0200

    wrong namespace

--- routes/microservice/authtentication.php
+++ routes/microservice/authtentication.php
@@ -2,14 +2,14 @@ use Illuminate\Support\Facades\\Illuminate\Support\Facades\Route;
 
 <?php
 
-\Illuminate\Support\Facades\Route::get('/check', [\Budgetcontrol\Authtentication\Controller\AuthController::class, 'check']);
-\Illuminate\Support\Facades\Route::get('/user-info', [\Budgetcontrol\Authtentication\Controller\AuthController::class, 'getUserInfo']);
-\Illuminate\Support\Facades\Route::post('/sign-up', [\Budgetcontrol\Authtentication\Controller\SignUpController::class, 'signUp']);
-\Illuminate\Support\Facades\Route::get('/confirm/{token}', [\Budgetcontrol\Authtentication\Controller\SignUpController::class, 'confirmToken']);
-\Illuminate\Support\Facades\Route::post('/authenticate', [\Budgetcontrol\Authtentication\Controller\LoginController::class, 'authenticate']);
-\Illuminate\Support\Facades\Route::post('/reset-password', [\Budgetcontrol\Authtentication\Controller\AuthController::class, 'resetPassword']);
-\Illuminate\Support\Facades\Route::post('/verify-email', [\Budgetcontrol\Authtentication\Controller\AuthController::class, 'sendVerifyEmail']);
-\Illuminate\Support\Facades\Route::put('/reset-password/{token}', [\Budgetcontrol\Authtentication\Controller\AuthController::class, 'resetPassword']);
-\Illuminate\Support\Facades\Route::get('/authenticate/{provider}', [\Budgetcontrol\Authtentication\Controller\ProviderController::class, 'authenticateProvider']);
-\Illuminate\Support\Facades\Route::get('/authenticate/token/{provider}', [\Budgetcontrol\Authtentication\Controller\ProviderController::class, 'providerToken']);
-\Illuminate\Support\Facades\Route::get('/logout', [\Budgetcontrol\Authtentication\Controller\AuthController::class, 'logout']);
+\Illuminate\Support\Facades\Route::get('/check', [\App\Http\Controllers\AuthController::class, 'check']);
+\Illuminate\Support\Facades\Route::get('/user-info', [\App\Http\Controllers\AuthController::class, 'getUserInfo']);
+\Illuminate\Support\Facades\Route::post('/sign-up', [\App\Http\Controllers\AuthController::class, 'signUp']);
+\Illuminate\Support\Facades\Route::get('/confirm/{token}', [\App\Http\Controllers\AuthController::class, 'confirmToken']);
+\Illuminate\Support\Facades\Route::post('/authenticate', [\App\Http\Controllers\AuthController::class, 'authenticate']);
+\Illuminate\Support\Facades\Route::post('/reset-password', [\App\Http\Controllers\AuthController::class, 'resetPassword']);
+\Illuminate\Support\Facades\Route::post('/verify-email', [\App\Http\Controllers\AuthController::class, 'sendVerifyEmail']);
+\Illuminate\Support\Facades\Route::put('/reset-password/{token}', [\App\Http\Controllers\AuthController::class, 'resetPassword']);
+\Illuminate\Support\Facades\Route::get('/authenticate/{provider}', [\App\Http\Controllers\AuthController::class, 'authenticateProvider']);
+\Illuminate\Support\Facades\Route::get('/authenticate/token/{provider}', [\App\Http\Controllers\AuthController::class, 'providerToken']);
+\Illuminate\Support\Facades\Route::get('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
