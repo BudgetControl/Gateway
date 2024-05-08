8c0ab1c1adb1cf75546a1964dfd8c10ff0f2e369
Marco De Felice <=>
2024-05-08 15:40:12 +0200

    budget apis

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -7,13 +7,174 @@ use Illuminate\Support\Facades\Http;
 
 class BudgetController extends Controller {
 
-    public function budgetsList(Request $request): Response
+    public function list(Request $request): Response
     {
         //get workspace uuid form headers
         $body = $request->all();
         $wsid = $body['token']['current_ws'];
         $basePath = $this->routes['budgets'];
-        $response = Http::get("$basePath/$wsid/list");
+        $response = Http::get("$basePath/$wsid");
+        $data = $response->json();
+        
+        if(json_encode($data) === null) {
+            return response("An error occurred", 500, ['Content-Type' => 'application/json']);
+        }
+        // Process the response
+        if ($response->successful()) {
+            $statusCode = $response->status();
+        } else {
+            // Handle the error
+            $statusCode = $response->status();
+            // Handle the error based on the status code
+        }
+
+        return response($data, $statusCode, ['Content-Type' => 'application/json']);
+    }
+
+    public function show(Request $request, $uuid): Response
+    {
+        $body = $request->all();
+        $wsid = $body['token']['current_ws'];
+        $basePath = $this->routes['budgets'];
+        $response = Http::get("$basePath/$wsid/$uuid");
+        $data = $response->json();
+        
+        if(json_encode($data) === null) {
+            return response("An error occurred", 500, ['Content-Type' => 'application/json']);
+        }
+        // Process the response
+        if ($response->successful()) {
+            $statusCode = $response->status();
+        } else {
+            // Handle the error
+            $statusCode = $response->status();
+            // Handle the error based on the status code
+        }
+
+        return response($data, $statusCode, ['Content-Type' => 'application/json']);
+    }
+
+    public function create(Request $request): Response
+    {
+        $body = $request->all();
+        $wsid = $body['token']['current_ws'];
+        $basePath = $this->routes['budgets'];
+        $response = Http::post("$basePath/$wsid", $body);
+        $data = $response->json();
+        
+        if(json_encode($data) === null) {
+            return response("An error occurred", 500, ['Content-Type' => 'application/json']);
+        }
+        // Process the response
+        if ($response->successful()) {
+            $statusCode = $response->status();
+        } else {
+            // Handle the error
+            $statusCode = $response->status();
+            // Handle the error based on the status code
+        }
+
+        return response($data, $statusCode, ['Content-Type' => 'application/json']);
+    }
+
+    public function update(Request $request, $uuid): Response
+    {
+        $body = $request->all();
+        $wsid = $body['token']['current_ws'];
+        $basePath = $this->routes['budgets'];
+        $response = Http::put("$basePath/$wsid/$uuid", $body);
+        $data = $response->json();
+        
+        if(json_encode($data) === null) {
+            return response("An error occurred", 500, ['Content-Type' => 'application/json']);
+        }
+        // Process the response
+        if ($response->successful()) {
+            $statusCode = $response->status();
+        } else {
+            // Handle the error
+            $statusCode = $response->status();
+            // Handle the error based on the status code
+        }
+
+        return response($data, $statusCode, ['Content-Type' => 'application/json']);
+    }
+
+    public function delete(Request $request, $uuid): Response
+    {
+        $body = $request->all();
+        $wsid = $body['token']['current_ws'];
+        $basePath = $this->routes['budgets'];
+        $response = Http::delete("$basePath/$wsid/$uuid");
+        $data = $response->json();
+        
+        if(json_encode($data) === null) {
+            return response("An error occurred", 500, ['Content-Type' => 'application/json']);
+        }
+        // Process the response
+        if ($response->successful()) {
+            $statusCode = $response->status();
+        } else {
+            // Handle the error
+            $statusCode = $response->status();
+            // Handle the error based on the status code
+        }
+
+        return response($data, $statusCode, ['Content-Type' => 'application/json']);
+    }
+
+    public function expired(Request $request, $uuid): Response
+    {
+        $body = $request->all();
+        $wsid = $body['token']['current_ws'];
+        $basePath = $this->routes['budgets'];
+        $response = Http::get("$basePath/$wsid/$uuid/expired");
+        $data = $response->json();
+        
+        if(json_encode($data) === null) {
+            return response("An error occurred", 500, ['Content-Type' => 'application/json']);
+        }
+        // Process the response
+        if ($response->successful()) {
+            $statusCode = $response->status();
+        } else {
+            // Handle the error
+            $statusCode = $response->status();
+            // Handle the error based on the status code
+        }
+
+        return response($data, $statusCode, ['Content-Type' => 'application/json']);
+    }
+
+    public function exceeded(Request $request, $uuid): Response
+    {
+        $body = $request->all();
+        $wsid = $body['token']['current_ws'];
+        $basePath = $this->routes['budgets'];
+        $response = Http::get("$basePath/$wsid/$uuid/exceeded");
+        $data = $response->json();
+        
+        if(json_encode($data) === null) {
+            return response("An error occurred", 500, ['Content-Type' => 'application/json']);
+        }
+        // Process the response
+        if ($response->successful()) {
+            $statusCode = $response->status();
+        } else {
+            // Handle the error
+            $statusCode = $response->status();
+            // Handle the error based on the status code
+        }
+
+        return response($data, $statusCode, ['Content-Type' => 'application/json']);
+    }
+
+    public function status(Request $request, $uuid): Response
+    {
+        $body = $request->all();
+        $wsid = $body['token']['current_ws'];
+        $basePath = $this->routes['budgets'];
+        $response = Http::get("$basePath/$wsid/$uuid/status");
         $data = $response->json();
         
         if(json_encode($data) === null) {
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/microservice/authtentication.php';
 require_once __DIR__ . '/microservice/stats.php';
 require_once __DIR__ . '/microservice/workspace.php';
+require_once __DIR__ . '/microservice/budget.php';
 
 \Illuminate\Support\Facades\Route::any('/{any}','\App\Http\Controllers\WorkspaceController@getRoutes')->where('any', '.*');
 
