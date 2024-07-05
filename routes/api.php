404a3bdb70462eed988741c95dc3a1393550a495
Marco De Felice <=>
2024-07-05 17:09:58 +0200

    include

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/microservice/stats.php';
 require_once __DIR__ . '/microservice/workspace.php';
 require_once __DIR__ . '/microservice/budget.php';
 require_once __DIR__ . '/microservice/searchengine.php';
+require_once __DIR__ . '/microservice/wallet.php';
 
 \Illuminate\Support\Facades\Route::get('/monitor/{ms}','\App\Http\Controllers\BaseController@monitor');
 \Illuminate\Support\Facades\Route::any('/{any}','\App\Http\Controllers\WorkspaceController@getRoutes')->where('any', '.*');
