d5c486a76d5a0bd39fa2a4c4b16c80dcf0abf283
Marco De Felice <=>
2024-07-08 11:39:26 +0200

    hotfix - delete api

--- routes/microservice/budget.php
+++ routes/microservice/budget.php
@@ -6,7 +6,7 @@
 \Illuminate\Support\Facades\Route::get('/budget/{uuid}', '\App\Http\Controllers\BudgetController@show');
 \Illuminate\Support\Facades\Route::post('/budget/create', '\App\Http\Controllers\BudgetController@create');
 \Illuminate\Support\Facades\Route::put('/budget/update/{uuid}', '\App\Http\Controllers\BudgetController@update');
-\Illuminate\Support\Facades\Route::delete('/budget/delete/{uuid}', '\App\Http\Controllers\BudgetController@delete');
+\Illuminate\Support\Facades\Route::delete('/budget/{uuid}', '\App\Http\Controllers\BudgetController@delete');
 \Illuminate\Support\Facades\Route::get('/budget/{uuid}/expired', '\App\Http\Controllers\BudgetController@expired');
 \Illuminate\Support\Facades\Route::get('/budget/{uuid}/exceeded', '\App\Http\Controllers\BudgetController@exceeded');
 \Illuminate\Support\Facades\Route::get('/budget/{uuid}/status', '\App\Http\Controllers\BudgetController@status');
