a94a11cc2e9ee21664be1a3b69074b5af9048b5e
Marco De Felice <=>
2024-07-11 12:30:11 +0200

    fix wallet endpoint

--- routes/microservice/wallet.php
+++ routes/microservice/wallet.php
@@ -2,12 +2,12 @@
 \Illuminate\Support\Facades\Route::group(['middleware' => [\App\Http\Middleware\AuthMiddleware::class]], function () {
 
 //# ########### STATS WalletS ###########
-\Illuminate\Support\Facades\Route::get('/list', '\App\Http\Controllers\WalletController@list');
-\Illuminate\Support\Facades\Route::get('/show/{uuid}', '\App\Http\Controllers\WalletController@show');
-\Illuminate\Support\Facades\Route::post('/create', '\App\Http\Controllers\WalletController@create');
-\Illuminate\Support\Facades\Route::put('/update/{uuid}', '\App\Http\Controllers\WalletController@update');
-\Illuminate\Support\Facades\Route::delete('/{uuid}', '\App\Http\Controllers\WalletController@delete');
-\Illuminate\Support\Facades\Route::patch('/restore/{uuid}', '\App\Http\Controllers\WalletController@restore');
-\Illuminate\Support\Facades\Route::patch('/sorting/{uuid}', '\App\Http\Controllers\WalletController@sorting');
+\Illuminate\Support\Facades\Route::get('/wallet/list', '\App\Http\Controllers\WalletController@list');
+\Illuminate\Support\Facades\Route::get('/wallet/show/{uuid}', '\App\Http\Controllers\WalletController@show');
+\Illuminate\Support\Facades\Route::post('/wallet/create', '\App\Http\Controllers\WalletController@create');
+\Illuminate\Support\Facades\Route::put('/wallet/update/{uuid}', '\App\Http\Controllers\WalletController@update');
+\Illuminate\Support\Facades\Route::delete('/wallet/{uuid}', '\App\Http\Controllers\WalletController@delete');
+\Illuminate\Support\Facades\Route::patch('/wallet/restore/{uuid}', '\App\Http\Controllers\WalletController@restore');
+\Illuminate\Support\Facades\Route::patch('/wallet/sorting/{uuid}', '\App\Http\Controllers\WalletController@sorting');
 
 });
