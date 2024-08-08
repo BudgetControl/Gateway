5dd8e721f30ec1bb4009ada46a2ae841710c0038
Marco De Felice <=>
2024-08-08 16:59:15 +0200

    routes

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ require_once __DIR__ . '/microservice/workspace.php';
 require_once __DIR__ . '/microservice/budget.php';
 require_once __DIR__ . '/microservice/searchengine.php';
 require_once __DIR__ . '/microservice/wallet.php';
+require_once __DIR__ . '/microservice/entry.php';
 
 \Illuminate\Support\Facades\Route::get('/monitor/{ms}','\App\Http\Controllers\BaseController@monitor');
-\Illuminate\Support\Facades\Route::any('/{any}','\App\Http\Controllers\WorkspaceController@getRoutes')->where('any', '.*');
--- routes/microservice/entry.php
+++ routes/microservice/entry.php
@@ -1,33 +1,31 @@
 <?php
 
+\Illuminate\Support\Facades\Route::get('/entry/expense', '\App\Http\Controllers\ExpensesController@list');
+\Illuminate\Support\Facades\Route::get('/entry/expense/{uuid}', '\App\Http\Controllers\EntryController@show');
+\Illuminate\Support\Facades\Route::post('/entry/expense', '\App\Http\Controllers\ExpensesController@create');
+\Illuminate\Support\Facades\Route::put('/entry/expense/{uuid}', '\App\Http\Controllers\ExpensesController@update');
 
+\Illuminate\Support\Facades\Route::get('/entry/income', '\App\Http\Controllers\IncomingController@list');
+\Illuminate\Support\Facades\Route::get('/entry/income/{uuid}', '\App\Http\Controllers\EntryController@show');
+\Illuminate\Support\Facades\Route::post('/entry/income', '\App\Http\Controllers\IncomingController@create');
+\Illuminate\Support\Facades\Route::put('/entry/income/{uuid}', '\App\Http\Controllers\IncomingController@update');
 
-\Illuminate\Support\Facades\Route::get('/entry/expense/{wsid}', \App\Http\Controllers\ExpensesController::class . ':get');
-\Illuminate\Support\Facades\Route::get('/entry/expense//{uuid}', \App\Http\Controllers\EntryController::class . ':show');
-\Illuminate\Support\Facades\Route::post('/entry/expense/{wsid}', \App\Http\Controllers\ExpensesController::class . ':create');
-\Illuminate\Support\Facades\Route::put('/entry/expense/{uuid}', \App\Http\Controllers\ExpensesController::class . ':update');
+\Illuminate\Support\Facades\Route::get('/entry/transfer', '\App\Http\Controllers\TransferController@list');
+\Illuminate\Support\Facades\Route::get('/entry/transfer/{uuid}', '\App\Http\Controllers\EntryController@show');
+\Illuminate\Support\Facades\Route::post('/entry/transfer', '\App\Http\Controllers\TransferController@create');
+\Illuminate\Support\Facades\Route::put('/entry/transfer/{uuid}', '\App\Http\Controllers\TransferController@update');
 
-\Illuminate\Support\Facades\Route::get('/entry/income/{wsid}', \App\Http\Controllers\IncomingController::class . ':get');
-\Illuminate\Support\Facades\Route::get('/entry/income/{uuid}', \App\Http\Controllers\EntryController::class . ':show');
-\Illuminate\Support\Facades\Route::post('/entry/income/{wsid}', \App\Http\Controllers\IncomingController::class . ':create');
-\Illuminate\Support\Facades\Route::put('/entry/income/{uuid}', \App\Http\Controllers\IncomingController::class . ':update');
+\Illuminate\Support\Facades\Route::get('/entry/debit', '\App\Http\Controllers\DebitController@list');
+\Illuminate\Support\Facades\Route::get('/entry/debit/{uuid}', '\App\Http\Controllers\EntryController@show');
+\Illuminate\Support\Facades\Route::post('/entry/debit', '\App\Http\Controllers\DebitController@create');
+\Illuminate\Support\Facades\Route::put('/entry/debit/{uuid}', '\App\Http\Controllers\DebitController@update');
 
-\Illuminate\Support\Facades\Route::get('/entry/transfer/{wsid}', \App\Http\Controllers\TransferController::class . ':get');
-\Illuminate\Support\Facades\Route::get('/entry/transfer/{uuid}', \App\Http\Controllers\EntryController::class . ':show');
-\Illuminate\Support\Facades\Route::post('/entry/transfer/{wsid}', \App\Http\Controllers\TransferController::class . ':create');
-\Illuminate\Support\Facades\Route::put('/entry/transfer/{uuid}', \App\Http\Controllers\TransferController::class . ':update');
+\Illuminate\Support\Facades\Route::delete('/entry/debit/{uuid}', '\App\Http\Controllers\DebitController@delete');
+\Illuminate\Support\Facades\Route::delete('/entry/income/{uuid}', '\App\Http\Controllers\IncomingController@delete');
+\Illuminate\Support\Facades\Route::delete('/entry/expense/{uuid}', '\App\Http\Controllers\ExpensesController@delete');
+\Illuminate\Support\Facades\Route::delete('/entry/transfer/{uuid}', '\App\Http\Controllers\TransferController@delete');
+\Illuminate\Support\Facades\Route::delete('/entry/{uuid}', '\App\Http\Controllers\EntryController@delete');
 
-\Illuminate\Support\Facades\Route::get('/entry/debit/{wsid}', \App\Http\Controllers\DebitController::class . ':get');
-\Illuminate\Support\Facades\Route::get('/entry/debit/{uuid}', \App\Http\Controllers\EntryController::class . ':show');
-\Illuminate\Support\Facades\Route::post('/entry/debit/{wsid}', \App\Http\Controllers\DebitController::class . ':create');
-\Illuminate\Support\Facades\Route::put('/entry/debit/{uuid}', \App\Http\Controllers\DebitController::class . ':update');
-
-\Illuminate\Support\Facades\Route::delete('/entry/debit/{uuid}', \App\Http\Controllers\DebitController::class . ':delete');
-\Illuminate\Support\Facades\Route::delete('/entry/income/{uuid}', \App\Http\Controllers\IncomingController::class . ':delete');
-\Illuminate\Support\Facades\Route::delete('/entry/expense/{uuid}', \App\Http\Controllers\ExpensesController::class . ':delete');
-\Illuminate\Support\Facades\Route::delete('/entry/transfer/{uuid}', \App\Http\Controllers\TransferController::class . ':delete');
-\Illuminate\Support\Facades\Route::delete('/entry/{uuid}', \App\Http\Controllers\EntryController::class . ':delete');
-
-\Illuminate\Support\Facades\Route::get('/entry/{uuid}', \App\Http\Controllers\EntryController::class . ':show');
-\Illuminate\Support\Facades\Route::put('/entry/{uuid}', \App\Http\Controllers\EntryController::class . ':update');
-\Illuminate\Support\Facades\Route::get('/entry/{wsid}', [\App\Http\Controllers\EntryController::class, 'list']);
+\Illuminate\Support\Facades\Route::get('/entry/{uuid}', '\App\Http\Controllers\EntryController@show');
+\Illuminate\Support\Facades\Route::put('/entry/{uuid}', '\App\Http\Controllers\EntryController@update');
+\Illuminate\Support\Facades\Route::get('/entry', '\App\Http\Controllers\EntryController@list');
