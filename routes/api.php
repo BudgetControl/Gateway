7e80613eab43e931e07e12fc69d762ed060383db
Marco De Felice <=>
2024-08-30 16:10:31 +0200

    wrong controller sorry

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ require_once __DIR__ . '/microservice/searchengine.php';
 require_once __DIR__ . '/microservice/wallet.php';
 require_once __DIR__ . '/microservice/entry.php';
 
-\Illuminate\Support\Facades\Route::get('/payment-types', '\App\Http\Controllers\BaseController@payment-types');
-\Illuminate\Support\Facades\Route::get('/currencies', '\App\Http\Controllers\BaseController@currencies');
-\Illuminate\Support\Facades\Route::get('/categories', '\App\Http\Controllers\BaseController@categories');
+\Illuminate\Support\Facades\Route::get('/payment-types', '\App\Http\Controllers\CoreController@payment-types');
+\Illuminate\Support\Facades\Route::get('/currencies', '\App\Http\Controllers\CoreController@currencies');
+\Illuminate\Support\Facades\Route::get('/categories', '\App\Http\Controllers\CoreController@categories');
 \Illuminate\Support\Facades\Route::get('/monitor/{ms}','\App\Http\Controllers\BaseController@monitor');
