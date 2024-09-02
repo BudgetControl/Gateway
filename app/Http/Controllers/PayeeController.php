4bb650d9f696603419945dd884e1c2400d1cd016
Marco De Felice <=>
2024-09-02 12:21:09 +0200

    some fixes on gateway api

--- app/Http/Controllers/PayeeController.php
+++ app/Http/Controllers/PayeeController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 
-class DebitController extends EntryController {
+class PayeeController extends EntryController {
 
     protected string $entryType = "/debit";
 
--- routes/microservice/entry.php
+++ routes/microservice/entry.php
@@ -17,12 +17,12 @@
     \Illuminate\Support\Facades\Route::post('/entry/transfer', '\App\Http\Controllers\TransferController@create');
     \Illuminate\Support\Facades\Route::put('/entry/transfer/{uuid}', '\App\Http\Controllers\TransferController@update');
 
-    \Illuminate\Support\Facades\Route::get('/entry/debit', '\App\Http\Controllers\DebitController@list');
+    \Illuminate\Support\Facades\Route::get('/entry/debit', '\App\Http\Controllers\PayeeController@list');
     \Illuminate\Support\Facades\Route::get('/entry/debit/{uuid}', '\App\Http\Controllers\EntryController@show');
-    \Illuminate\Support\Facades\Route::post('/entry/debit', '\App\Http\Controllers\DebitController@create');
-    \Illuminate\Support\Facades\Route::put('/entry/debit/{uuid}', '\App\Http\Controllers\DebitController@update');
+    \Illuminate\Support\Facades\Route::post('/entry/debit', '\App\Http\Controllers\PayeeController@create');
+    \Illuminate\Support\Facades\Route::put('/entry/debit/{uuid}', '\App\Http\Controllers\PayeeController@update');
 
-    \Illuminate\Support\Facades\Route::delete('/entry/debit/{uuid}', '\App\Http\Controllers\DebitController@delete');
+    \Illuminate\Support\Facades\Route::delete('/entry/debit/{uuid}', '\App\Http\Controllers\PayeeController@delete');
     \Illuminate\Support\Facades\Route::delete('/entry/income/{uuid}', '\App\Http\Controllers\IncomingController@delete');
     \Illuminate\Support\Facades\Route::delete('/entry/expense/{uuid}', '\App\Http\Controllers\ExpensesController@delete');
     \Illuminate\Support\Facades\Route::delete('/entry/transfer/{uuid}', '\App\Http\Controllers\TransferController@delete');
