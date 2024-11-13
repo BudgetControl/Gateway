37e8eb10072ceed8777755097e38c863f812809d
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2024-11-13 14:54:57 +0100

    feat - new route

--- routes/microservice/debt.php
+++ routes/microservice/debt.php
@@ -3,6 +3,7 @@
 \Illuminate\Support\Facades\Route::group(['middleware' => [\App\Http\Middleware\AuthMiddleware::class]], function () {
 
     \Illuminate\Support\Facades\Route::get('/payees', '\App\Http\Controllers\DebtController@payeeList');
+    \Illuminate\Support\Facades\Route::get('/debits', '\App\Http\Controllers\DebtController@getDebits');
     \Illuminate\Support\Facades\Route::delete('/debt/{uuid}', '\App\Http\Controllers\DebtController@deleteDebt');
 
 });
