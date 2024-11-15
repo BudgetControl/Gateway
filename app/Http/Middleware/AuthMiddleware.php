3fa1711009d81f6443def7566c3c34953fc0bfba
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2024-11-15 09:21:19 +0100

    restore

--- app/Http/Middleware/AuthMiddleware.php
+++ app/Http/Middleware/AuthMiddleware.php
@@ -17,8 +17,30 @@ class AuthMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        
-        $request->merge(['token' => "d"]);
+        Log::debug('Request: ' . $request->fullUrl());
+        Log::debug('Headers: ' . json_encode($request->header()));
+        Log::debug('Body: ' . json_encode($request->all()));
+
+        $token = $request->header('X-BC-Token');
+        $authToken = $request->header('Authorization');
+
+        if(empty($token) || empty($authToken)) {
+            return response('Unauthorized', 401);
+        }
+
+        if (JwtService::isValidToken($token) === false) {
+            return response('Unauthorized', 401);
+        }
+
+        // Validate the token
+        try {
+            $decoded = JwtService::decodeToken($token);
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            return response('Unauthorized', 401);
+        }
+
+        $request->merge(['token' => $decoded]);
         return $next($request);
     }
 }
