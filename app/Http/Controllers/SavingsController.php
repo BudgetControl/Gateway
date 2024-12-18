8f0b4fb539b3787bc30ec1d4cde8be68fb3e1334
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2024-12-18 18:00:04 +0100

    added new service saving

--- app/Http/Controllers/SavingsController.php
+++ app/Http/Controllers/SavingsController.php
@@ -9,13 +9,63 @@ use App\Facade\MicroserviceCLient;
 
 class SavingsController extends Controller {
 
-    public function get(Request $request, $uuid)
+    private function getWorkspaceId($token)
+    {
+        return Workspace::where('uuid', $token['current_ws'])->first()->id;
+    }
+
+    private function getService()
+    {
+        return MicroserviceCLient::saving();
+    }
+
+    public function list(Request $request)
     {
         $body = $request->all();
-        $wsid = Workspace::where('uuid', $body['token']['current_ws'])->first()->id;
+        $wsid = $this->getWorkspaceId($body['token']);
         $queryParams = $request->query();
 
-        $service = MicroserviceCLient::saving();
-        $service->getSaving($uuid);
+        $service = $this->getService();
+        return $service->list($wsid);
+    }
+
+    public function show(Request $request, $uuid)
+    {
+        $body = $request->all();
+        $wsid = $this->getWorkspaceId($body['token']);
+        $queryParams = $request->query();
+
+        $service = $this->getService();
+        return $service->getSaving($uuid);
+    }
+
+    public function create(Request $request)
+    {
+        $body = $request->all();
+        $wsid = $this->getWorkspaceId($body['token']);
+        $queryParams = $request->query();
+
+        $service = $this->getService();
+        return $service->create($wsid, $body);
+    }
+
+    public function update(Request $request, $uuid)
+    {
+        $body = $request->all();
+        $wsid = $this->getWorkspaceId($body['token']);
+        $queryParams = $request->query();
+
+        $service = $this->getService();
+        return $service->updateSaving($wsid, $uuid, $body);
+    }
+
+    public function delete(Request $request, $uuid)
+    {
+        $body = $request->all();
+        $wsid = $this->getWorkspaceId($body['token']);
+        $queryParams = $request->query();
+
+        $service = $this->getService();
+        return $service->deleteSaving($wsid, $uuid);
     }
 }
