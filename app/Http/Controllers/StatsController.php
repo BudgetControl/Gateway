d22d9fdf10b5b98db24453f3d5f4ec890def44f3
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2025-01-10 13:47:25 +0100

    feat: added new route

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -83,6 +83,56 @@ class StatsController extends Controller {
         return response($data, $statusCode, ['Content-Type' => 'application/json']);
     }
 
+    public function debitsTotalNegative(Request $request): Response
+    {
+        //get workspace uuid form headers
+        $body = $request->all();
+        $wsid = $body['token']['current_ws'];
+        $basePath = $this->routes['stats'];
+        $response = Http::get("$basePath/$wsid/debits/total-negative");
+        $data = $response->json();
+        
+        if(json_encode($data) === null) {
+            Log::error('Error: on debits total negative', ['response' => $response->json()]);
+            return response("An error occurred", $response->status(), ['Content-Type' => 'application/json']);
+        }
+        // Process the response
+        if ($response->successful()) {
+            $statusCode = $response->status();
+        } else {
+            // Handle the error
+            $statusCode = $response->status();
+            // Handle the error based on the status code
+        }
+
+        return response($data, $statusCode, ['Content-Type' => 'application/json']);
+    }
+
+    public function debitsTotalPositive(Request $request): Response
+    {
+        //get workspace uuid form headers
+        $body = $request->all();
+        $wsid = $body['token']['current_ws'];
+        $basePath = $this->routes['stats'];
+        $response = Http::get("$basePath/$wsid/debits/total-positive");
+        $data = $response->json();
+        
+        if(json_encode($data) === null) {
+            Log::error('Error: on debits total positive', ['response' => $response->json()]);
+            return response("An error occurred", $response->status(), ['Content-Type' => 'application/json']);
+        }
+        // Process the response
+        if ($response->successful()) {
+            $statusCode = $response->status();
+        } else {
+            // Handle the error
+            $statusCode = $response->status();
+            // Handle the error based on the status code
+        }
+
+        return response($data, $statusCode, ['Content-Type' => 'application/json']);
+    }
+
     public function total(Request $request): Response
     {
         //get workspace uuid form headers
--- routes/microservice/stats.php
+++ routes/microservice/stats.php
@@ -11,6 +11,8 @@
     \Illuminate\Support\Facades\Route::get('/stats/total/planned', '\App\Http\Controllers\StatsController@totalPlanned');
     \Illuminate\Support\Facades\Route::post('/stats/entries', '\App\Http\Controllers\StatsController@entries');
     \Illuminate\Support\Facades\Route::get('/stats/debits', '\App\Http\Controllers\StatsController@debits');
+    \Illuminate\Support\Facades\Route::get('/stats/debits/total-negative', '\App\Http\Controllers\StatsController@debitsTotalNegative');
+    \Illuminate\Support\Facades\Route::get('/stats/debits/total-positive', '\App\Http\Controllers\StatsController@debitsTotalPositive');
 
     // ########### STATS CHART ###########
     \Illuminate\Support\Facades\Route::get('/stats/chart/line/incoming-expenses', '\App\Http\Controllers\ChartsController@incomingExpensesLineByDate')->middleware([\App\Http\Middleware\CachingMiddleware::class . ':' . 60]);
