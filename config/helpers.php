1bbe3fd38a1ef8412563d815b9f7e760ad0bcdc1
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2025-07-18 17:32:11 +0200

    other fix

--- config/helpers.php
+++ config/helpers.php
@@ -1,31 +1,36 @@
 <?php
+
 /**
  * This file is a helper file that contains various functions.
  */
 
-if(!function_exists('config')) {
-    function confing(string $key, string $value): string {
+if (!function_exists('config')) {
+    function confing(string $key, string $value): string
+    {
         return $_ENV[$key] ?? $value;
     }
 }
 
-if(!function_exists('response')) {
-    function response(array $dataResponse, int $statusCode = 200, array $headers=[]): \Psr\Http\Message\ResponseInterface {
+if (!function_exists('response')) {
+    function response(?array $dataResponse, int $statusCode = 200, array $headers = []): \Psr\Http\Message\ResponseInterface
+    {
         $response = new \Slim\Psr7\Response();
 
-        $jsonData = json_encode($dataResponse);
-        if ($jsonData === false) {
-            $errorResponse = new \Slim\Psr7\Response();
-            $errorResponse->getBody()->write('Errore nella codifica JSON dei dati');
-            return $errorResponse->withStatus(500);
+        if (!is_null($dataResponse)) {
+            $jsonData = json_encode($dataResponse);
+            if ($jsonData === false) {
+                $errorResponse = new \Slim\Psr7\Response();
+                $errorResponse->getBody()->write('Errore nella codifica JSON dei dati');
+                return $errorResponse->withStatus(500);
+            }
+
+            $response->getBody()->write($jsonData);
         }
-        
-        $response->getBody()->write($jsonData);
 
         foreach ($headers as $key => $value) {
             $response = $response->withHeader($key, $value);
         }
-        
+
         return $response->withHeader('Content-Type', 'application/json')->withStatus($statusCode);
     }
 }
@@ -34,27 +39,29 @@ if(!function_exists('response')) {
  * Checks if the function 'cacheKey_refreshToken' exists.
  * If it does not exist, it can be defined to avoid redeclaration errors.
  */
-if(!function_exists('cacheKey_refreshToken')) {
-    function cacheKey_refreshToken(string $username): string {
-        return md5($username.'refresh_token');
+if (!function_exists('cacheKey_refreshToken')) {
+    function cacheKey_refreshToken(string $username): string
+    {
+        return md5($username . 'refresh_token');
     }
 }
 
-if(!function_exists('cache_tags_mapping')) {
-    function cache_tags_mapping(): array {
+if (!function_exists('cache_tags_mapping')) {
+    function cache_tags_mapping(): array
+    {
         return [
-                'budget' => ['stats', 'entry', 'budget'],
-                'entry' => ['stats', 'budget', 'wallet', 'find', 'entry'],
-                'wallet' => ['stats', 'wallet'],
-                'label' => ['entry','stats', 'label'],
-                'goals' => ['stats', 'goals'],
-                'debt' => ['stats', 'entry', 'wallet', 'debt'],
-                'payees' => ['stats', 'entry', 'debt', 'wallet', 'payees'],
-                'workspace' => ['workspace'],
-                'find' => ['find'],
-                'stats' => ['budget', 'entry', 'wallet', 'label', 'goals', 'debt', 'workspace','stats', 'find'],
-                'all' => ['stats', 'budget', 'entry', 'wallet', 'label', 'goals', 'debt', 'workspace', 'find'],
-                'auth' => ['stats', 'workspace', 'find', 'auth'],
-            ];
+            'budget' => ['stats', 'entry', 'budget'],
+            'entry' => ['stats', 'budget', 'wallet', 'find', 'entry'],
+            'wallet' => ['stats', 'wallet'],
+            'label' => ['entry', 'stats', 'label'],
+            'goals' => ['stats', 'goals'],
+            'debt' => ['stats', 'entry', 'wallet', 'debt'],
+            'payees' => ['stats', 'entry', 'debt', 'wallet', 'payees'],
+            'workspace' => ['workspace'],
+            'find' => ['find'],
+            'stats' => ['budget', 'entry', 'wallet', 'label', 'goals', 'debt', 'workspace', 'stats', 'find'],
+            'all' => ['stats', 'budget', 'entry', 'wallet', 'label', 'goals', 'debt', 'workspace', 'find'],
+            'auth' => ['stats', 'workspace', 'find', 'auth'],
+        ];
     }
 }
