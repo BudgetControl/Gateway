5d120c03affc5d33d3d4997b05cb7c9d5c0cf753
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2025-08-22 08:03:19 +0200

    try to move send contact email external

--- routes/microservice/notification.php
+++ routes/microservice/notification.php
@@ -1,8 +1,8 @@
 <?php
+$group->post('/notify/email/contact', [\Budgetcontrol\Gateway\Http\Controllers\NotificationController::class, 'sendEmail']);
 
 $app->group('/api', function ($group) {
 
-    $group->post('/notify/email/contact', [\Budgetcontrol\Gateway\Http\Controllers\NotificationController::class, 'sendEmail']);
     $group->post('/notify/email/auth/recovery-password', [\Budgetcontrol\Gateway\Http\Controllers\NotificationController::class, 'recoveryPassword']);
     $group->post('/notify/email/auth/sign-up', [\Budgetcontrol\Gateway\Http\Controllers\NotificationController::class, 'signUp']);
     $group->post('/notify/email/budget/exceeded', [\Budgetcontrol\Gateway\Http\Controllers\NotificationController::class, 'budgetExceeded']);
